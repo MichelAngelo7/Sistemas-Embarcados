7d53174e0bad35505072b76ed10f4c59ee50da6e
MichelAngelo7 <m_che_l@hotmail.com>
2020-03-03 16:47:48 -0300

    Display de 7 Seguimentos com potenciometro regulando velocidade

---
+++ display_7_seguimentos_com_pot.ino
@@ -0,0 +1,200 @@
+
+//
+const int a = 2;
+const int b = 3;
+const int c = 4;
+const int d = 5;
+const int e = 6;
+const int f = 7;
+const int g = 8;
+const int dp = 9;
+const int pot = A0;
+
+
+
+
+void setup(){
+  pinMode(a, OUTPUT);
+  pinMode(b, OUTPUT);
+  pinMode(c, OUTPUT);
+  pinMode(d, OUTPUT);
+  pinMode(e, OUTPUT);
+  pinMode(f, OUTPUT);
+  pinMode(g, OUTPUT);
+  pinMode(dp, OUTPUT);
+  pinMode(pot, INPUT);
+  Serial.begin(9600);
+
+  
+}
+
+void loop()
+{
+  //Ajusta o delay global 
+const int delay2 = analogRead(pot);
+  // Codigo para testar o display
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+  digitalWrite(dp, HIGH);
+  delay(delay2);
+  digitalWrite(a, LOW);
+  digitalWrite(b, LOW);
+  digitalWrite(c, LOW);
+  digitalWrite(d, LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(f, LOW);
+  digitalWrite(g, LOW);
+  digitalWrite(dp, LOW);
+  delay(delay2);
+  
+  // Codigo para o 0
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  delay(delay2);
+  digitalWrite(a, LOW);
+  digitalWrite(b, LOW);
+  digitalWrite(c, LOW);
+  digitalWrite(d, LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(f, LOW);
+  delay(delay2);
+
+  //codigo para o 1
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  delay(delay2);
+  digitalWrite(b, LOW);
+  digitalWrite(c, LOW);
+  delay(delay2);
+
+  //codigo para o 2
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(g, HIGH);
+  delay(delay2);
+  digitalWrite(a, LOW);
+  digitalWrite(b, LOW);
+  digitalWrite(d, LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(g, LOW);
+  delay(delay2);
+
+  //codigo para o 3
+
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(g, HIGH);
+  delay(delay2);
+  digitalWrite(a, LOW);
+  digitalWrite(b, LOW);
+  digitalWrite(c, LOW);
+  digitalWrite(d, LOW);
+  digitalWrite(g, LOW);
+  delay(delay2);
+
+  // codigo para o 4
+
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+  delay(delay2);
+  digitalWrite(b, LOW);
+  digitalWrite(c, LOW);
+  digitalWrite(f, LOW);
+  digitalWrite(g, LOW);
+  delay(delay2);
+
+  // codigo para 5 
+
+  digitalWrite(a, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+  delay(delay2);
+  digitalWrite(a, LOW);
+  digitalWrite(c, LOW);
+  digitalWrite(d, LOW);
+  digitalWrite(f, LOW);
+  digitalWrite(g, LOW);
+  delay(delay2);
+
+  // codigo para o 6 
+
+  digitalWrite(a, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+  delay(delay2);
+  digitalWrite(a, LOW);
+  digitalWrite(c, LOW);
+  digitalWrite(d, LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(f, LOW);
+  digitalWrite(g, LOW);
+  delay(delay2);
+
+  // codigo para o 7
+
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  
+  delay(delay2);
+  digitalWrite(a, LOW);
+  digitalWrite(b, LOW);
+  digitalWrite(c, LOW);
+  
+  delay(delay2);
+
+  // Codigo para o 8
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+  delay(delay2);
+  digitalWrite(a, LOW);
+  digitalWrite(b, LOW);
+  digitalWrite(c, LOW);
+  digitalWrite(d, LOW);
+  digitalWrite(e, LOW);
+  digitalWrite(f, LOW);
+  digitalWrite(g, LOW);
+  delay(delay2);
+
+  // Codigo para o 9
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+  delay(delay2);
+  digitalWrite(a, LOW);
+  digitalWrite(b, LOW);
+  digitalWrite(c, LOW);
+  digitalWrite(d, LOW);
+  digitalWrite(f, LOW);
+  digitalWrite(g, LOW);
+  delay(delay2);
+  
+}
